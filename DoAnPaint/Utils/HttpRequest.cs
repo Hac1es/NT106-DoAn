3a6cd076c4f35250532926715fe113a17775a7c0
Nguyễn Viết Tùng <nchinhtung@gmail.com>
2024-12-28 22:51:57 +0700

    Add project files.

---
+++ DoAnPaint/Utils/HttpRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnPaint.Utils
+{
+    public class HttpRequest //Singleton
+    {
+        private static readonly Lazy<HttpRequest> _instance = new Lazy<HttpRequest>(() => new HttpRequest());
+        private readonly HttpClient httpClient;
+
+        // Constructor private để ngăn tạo instance bên ngoài
+        private HttpRequest()
+        {
+            httpClient = new HttpClient();
+        }
+
+        // Instance được tạo lazily và thread-safe
+        public static HttpRequest Instance => _instance.Value;
+    }
+}
---
+++ SignalRServer/Program.cs
@@ -0,0 +1,48 @@
+using SignalRServer.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Lắng nghe trên tất cả các địa chỉ IP và cổng 7183
+    options.Listen(System.Net.IPAddress.Any, 7183, listenOptions =>
+    {
+        // Sử dụng HTTPS với chứng chỉ tự ký
+        listenOptions.UseHttps(@"C:\Users\nchin\Downloads\SelfSignedCert.pfx", "YourPassword");
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR(options =>
+{
+    options.MaximumReceiveMessageSize = 1024 * 1024;
+});
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<DreawHub>("api/hub");
+
+app.MapReverseProxy();
+
+app.Run();
